6eb1a9f7bb88d81efbb956f7bf9091e5e65ea8e2
Yves-Eric Martin <yemartin@mediweb.jp>
2017-05-16 12:24:23 +0900

    Rule of threes: refactor.

--- math_extensions.rb
+++ math_extensions.rb
@@ -1,25 +1,21 @@
 module Math
   def self.finite_log(x)
-    if x == 0
-      # Prevent the function from returning -Infinity
-      raise ArgumentError, 'would result in -Infinity'
-    end
-    Math.log(x)
+    self.finite_logn(x, Math::E)
   end
 
   def self.finite_log2(x)
-    if x == 0
-      # Prevent the function from returning -Infinity
-      raise ArgumentError, 'would result in -Infinity'
-    end
-    Math.log2(x)
+    self.finite_logn(x, 2)
   end
 
   def self.finite_log10(x)
+    self.finite_logn(x, 10)
+  end
+
+  def self.finite_logn(x, n)
     if x == 0
       # Prevent the function from returning -Infinity
       raise ArgumentError, 'would result in -Infinity'
     end
-    Math.log10(x)
+    Math.log(x, n)
   end
 end
