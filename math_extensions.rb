16684926eb72627e9b0903e75f3d32019499e05b
Yves-Eric Martin <yemartin@mediweb.jp>
2017-05-16 13:02:42 +0900

    Replace more code with metaprogramming.

--- math_extensions.rb
+++ math_extensions.rb
@@ -1,18 +1,15 @@
 module Math
   def self.method_missing(name, *args, &block)
-    if name.to_s =~ /^finite_log(\d+)$/
-      self.finite_logn(args[0], $1.to_i)
+    if name.to_s =~ /^finite_log(\d*)$/
+      self.finite_logn(args[0], $1)
     else
       super
     end
   end
 
-  def self.finite_log(x)
-    self.finite_logn(x, Math::E)
-  end
-
   private
   def self.finite_logn(x, n)
+    n = n == '' ? Math::E : n.to_i
     raise ArgumentError, 'would result in -Infinity' if x == 0
     raise ArgumentError, 'would result in Infinity' if x == Float::INFINITY
     Math.log(x, n)
